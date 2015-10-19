29df953773cd7f1134c394823da38933e68f6143
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2015-10-19 12:54:53 +0200

    #14: Create feature styler (https://github.com/Polymap4/polymap4-p4/issues/issue/14)
    support polyline in progress

--- org.polymap.p4/src/org/polymap/p4/project/ProjectInfoPanel.java
+++ org.polymap.p4/src/org/polymap/p4/project/ProjectInfoPanel.java
@@ -114,7 +114,7 @@ public class ProjectInfoPanel
                         @Override
                         public void widgetSelected( SelectionEvent e ) {
                             SimpleDialog dialog = site().toolkit().createSimpleDialog( "Titel!" );
-                            dialog.setContents( parent -> {} );
+                            dialog.setContents( p -> {} );
                             dialog.addAction( new Action( "OK" ) {
                                 @Override
                                 public void run() {
